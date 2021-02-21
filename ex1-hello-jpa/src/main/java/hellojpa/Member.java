f958bf85b7f2314506a2040dd427837a71b0aeb5
JungwooSim <cscd053@gmail.com>
2021-02-21 16:15:35 +0900

    일대일(1:1) 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,7 +1,13 @@
 package hellojpa;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Setter
+@Getter
 @Entity
 public class Member {
 
@@ -12,44 +18,13 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-
     @ManyToOne
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
-//    @Column(name = "TEAM_ID")
-//    private Long teamId;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-//    public Long getTeamId() {
-//        return teamId;
-//    }
-
-//    public void setTeamId(Long teamId) {
-//        this.teamId = teamId;
-//    }
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
 
     /*
     @Id
